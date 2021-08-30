f99286f8b4d4d0c8cb3a1021feeb1ba2f2432176
Aziz K <cosmonaut.aziz@gmail.com>
2021-08-30 10:47:48 +0600

    form bugs

---
+++ main.js
@@ -0,0 +1,12 @@
+// $.ajax({
+//     method: 'POST',
+//     url: 'https://formsubmit.co/ajax/your@email.com',
+//     dataType: 'json',
+//     accepts: 'application/json',
+//     data: {
+//         name: "FormSubmit",
+//         message: "I'm from Devro LABS"
+//     },
+//     success: (data) => console.log(data),
+//     error: (err) => console.log(err)
+// });
